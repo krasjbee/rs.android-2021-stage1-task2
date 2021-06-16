994d9a0c29bac93a8151a5ee0362dc09271f1802
krasjbee <zloyjb@gmail.com>
2021-06-16 23:13:33 +0700

    feat: task 2 finished

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,27 @@
 package subtask2
 
+import java.lang.StringBuilder
+
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        //quick check if a contains whole b
+        if (a.contains(b, ignoreCase = true)) return "YES"
+        //if a does not contain whole b, check if we can modify it
+        var aCounter = 0
+        var bCounter = 0
+        val outString = StringBuilder()
+        while (aCounter < a.length && bCounter < b.length) {
+            if (a[aCounter].equals(b[bCounter], ignoreCase = true)) {
+                outString.append(a[aCounter].toUpperCase())
+                aCounter++
+                bCounter++
+            } else {
+                aCounter++
+            }
+            if (outString.toString() == b) return "YES"
+        }
+        //if there was no return, a can't be modified to b , so return "NO"
+        return "NO"
     }
 }
