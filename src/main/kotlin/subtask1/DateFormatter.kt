56c99c3204d9f7e2f968b9f3c9972bd795197344
krasjbee <zloyjb@gmail.com>
2021-06-16 23:44:52 +0700

    feat: task 1 finished

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,29 @@
 package subtask1
 
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val calendar = GregorianCalendar.getInstance()
+        calendar.set(
+            year.toInt(),
+            //i don't know why the hell does it work this way
+            month.toInt()-1,
+            day.toInt()
+        )
+        //set calendar to strict mode
+        calendar.isLenient=false
+        //d - day of month (numeric)
+        //M - month (for four M it's word format)
+        //E - day of week (for four E it's word format)
+        val format = SimpleDateFormat("dd MMMM, EEEE", Locale("ru"))
+        return try {
+            format.format(calendar.time)
+        } catch (e:Exception){
+            "Такого дня не существует"
+        }
     }
 }
